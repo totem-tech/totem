74b8871db81d99fd0e7598fdbe399e10b1181fce
Chris D'Costa <chris.dcosta@totemaccounting.com>
2021-12-31 14:38:48 +0100

    Update genesis state, wasm and relabel obsolete Dockerfile

--- scripts/extraction.sh
+++ scripts/extraction.sh
@@ -3,12 +3,12 @@
 # This script should be run on Unix/Linux based systems
 # This is likely only to be required to run once but can be repurposed for other parachains.
 
-./target/release/parachain-totem-kapex-node export-genesis-state > ./res/genesis-states/kapex-genesis-state && \
-./target/release/parachain-totem-wapex-node export-genesis-state > ./res/genesis-states/wapex-genesis-state && \
-./target/release/parachain-totem-lego-node export-genesis-state > ./res/genesis-states/lego-genesis-state && \
-./target/release/parachain-totem-kapex-node export-genesis-wasm > ./res/wasm-blobs/kapex-genesis-wasm && \
-./target/release/parachain-totem-wapex-node export-genesis-wasm > ./res/wasm-blobs/wapex-genesis-wasm && \
-./target/release/parachain-totem-lego-node export-genesis-wasm > ./res/wasm-blobs/lego-genesis-wasm && \
+./target/release/parachain-totem-kapex-node export-genesis-state > ./res/genesis-states/kapex-genesis-state-new && \
+./target/release/parachain-totem-wapex-node export-genesis-state > ./res/genesis-states/wapex-genesis-state-new && \
+./target/release/parachain-totem-lego-node export-genesis-state > ./res/genesis-states/lego-genesis-state-new && \
+./target/release/parachain-totem-kapex-node export-genesis-wasm > ./res/wasm-blobs/kapex-genesis-wasm-new && \
+./target/release/parachain-totem-wapex-node export-genesis-wasm > ./res/wasm-blobs/wapex-genesis-wasm-new && \
+./target/release/parachain-totem-lego-node export-genesis-wasm > ./res/wasm-blobs/lego-genesis-wasm-new && \
 ./target/release/parachain-totem-kapex-node build-spec > ./res/kapex-latest.json && \
 ./target/release/parachain-totem-wapex-node build-spec > ./res/wapex-latest.json && \
 ./target/release/parachain-totem-lego-node build-spec > ./res/lego-latest.json && \
